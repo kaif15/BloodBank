fc33cb303c2109b54472620226d4d7ea6a4b9b54
Fauzul Kabir Chowdhury <fauzulkc@gmail.com>
2016-12-24 03:16:16 +0600

    FrontEnd Files
    All Front End Files have been added. Php, CSS, etc

---
+++ header.php
@@ -0,0 +1,20 @@
+<header class="clearfix">
+    <div class="container">
+            <div class="header-left">
+                <h1>Bangabandhu Seikh Mujib Medical University</h1>
+            </div>
+            <div class="header-right">
+                <label for="open">
+                    <span class="hidden-desktop"></span>
+                </label>
+                <input type="checkbox" name="" id="open">
+                <nav>
+                    <a <?php echo ($page == 'one') ? "class='active'" : ""; ?> href="index.php">Login</a>
+                    <a <?php echo ($page == 'two') ? "class='active'" : ""; ?> href="Blood_Request_Form.php">Blood Requisition Form</a>
+                    <a <?php echo ($page == 'three') ? "class='active'" : ""; ?> href="Compatibility_Report.php">Compatibility Report</a>
+                    <a href="#4">Doner Form</a>
+                    <a href="#5">Help</a>
+                </nav>
+            </div>
+        </div>
+    </header>
